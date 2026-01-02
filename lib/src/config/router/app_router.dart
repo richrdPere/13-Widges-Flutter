8f8587c1cd8954fe819c7ea2c87a4e8a66e9ab62
richrdPere <richrdpere1234@gmail.com>
2026-01-02 17:55:30 -0500

    snackbars dialogs terminado

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -36,5 +36,11 @@ final appRouter = GoRouter(
       name: ProgressScreen.name,
       builder: (context, state) => const ProgressScreen(),
     ),
+
+    GoRoute(
+      path: '/snackbars',
+      name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(),
+    ),
   ],
 );
--- lib/src/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/src/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,13 +1,82 @@
 import 'package:flutter/material.dart';
 
 class SnackbarScreen extends StatelessWidget {
-
   static const name = 'snackbar_screen';
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hola Mundo!'),
+      action: SnackBarAction(
+        label: 'Ok!',
+        onPressed: () {
+          // Some code to undo the change.
+        },
+      ),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás seguro?'),
+        content: const Text('Este acción no se puede deshacer'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancelar'),
+          ),
+          FilledButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Snackbars y Diálogos')),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('App de ejemplo para el curso de Flutter'),
+                  ],
+                );
+              },
+              child: const Text('Licencias usadas'),
+            ),
+
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar diálogo'),
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackbar(context),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        label: Text('Mostrar Snackbar'),
+      ),
+    );
   }
 }
